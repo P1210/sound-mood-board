0549a4ae78d3438c46cdff9028a99ba8619f5402
Pranjal Gupta <66200797+P1210@users.noreply.github.com>
2025-04-23 15:17:58 +0530

    Upload UI pages

--- src/pages/SoundView.jsx
+++ src/pages/SoundView.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import CardCarousel from "../components/cardCarousel";
 
+function SoundView() {
+  return (
+    <div className="w-full pt-4 pb-8 px-16">
+      <CardCarousel />
+    </div>
+  );
+}
+
+export default SoundView;
