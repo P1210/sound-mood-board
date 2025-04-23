7ad2256760b57c493233a4a7eabb626b35922435
Pranjal Gupta <66200797+P1210@users.noreply.github.com>
2025-04-23 15:20:47 +0530

    Add config data for the sound presets

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -1,1 +1,74 @@
+import fireplace from "../assets/sounds/fireplace-with-crackling-sounds-2-min-rk-178392.mp3";
+import rain from "../assets/sounds/rainy-day-in-town-with-birds-singing-194011.mp3";
+import ocean_waves from "../assets/sounds/gentle-ocean-waves-birdsong-and-gull-7109.mp3";
+import beach from "../assets/sounds/sandy-beach-calm-waves-water-nature-sounds-8052.mp3";
+import wind from "../assets/sounds/wind-in-trees-117477.mp3";
+import night from "../assets/sounds/night-ambience-17064.mp3";
+import soft_piano from "../assets/sounds/soft-piano-100-bpm-121529.mp3";
 
+import fireplace_image from "../assets/backgrounds/fireplace.jpg";
+import rain_image from "../assets/backgrounds/rain.jpg";
+import ocean_waves_image from "../assets/backgrounds/waves.jpg";
+import beach_image from "../assets/backgrounds/beach.jpg";
+import wind_image from "../assets/backgrounds/wind.jpg";
+import night_image from "../assets/backgrounds/night.jpg";
+import soft_piano_image from "../assets/backgrounds/soft-piano.jpg";
+
+export const soundTypes = [
+  {
+    soundKey: "rain",
+    label: "Rain pouring in backyard",
+    soundFile: rain,
+    backgroundImage: rain_image,
+    icon: "",
+    musicAttribute: "",
+  },
+  {
+    soundKey: "fireplace",
+    label: "Cozy Warm Fire place",
+    soundFile: fireplace,
+    backgroundImage: fireplace_image,
+    icon: "",
+    musicAttribute: "",
+  },
+  {
+    soundKey: "ocean_waves",
+    label: "Gentle Ocean Waves",
+    soundFile: ocean_waves,
+    backgroundImage: ocean_waves_image,
+    icon: "",
+    musicAttribute: "",
+  },
+  {
+    soundKey: "beach",
+    label: "Sandy beaches",
+    soundFile: beach,
+    backgroundImage: beach_image,
+    icon: "",
+    musicAttribute: "",
+  },
+  {
+    soundKey: "wind",
+    label: "Wind among leaves",
+    soundFile: wind,
+    backgroundImage: wind_image,
+    icon: "",
+    musicAttribute: "",
+  },
+  {
+    soundKey: "night",
+    label: "Night ambience",
+    soundFile: night,
+    backgroundImage: night_image,
+    icon: "",
+    musicAttribute: "",
+  },
+  {
+    soundKey: "soft_piano",
+    label: "Soft Lo-fi Music",
+    soundFile: soft_piano,
+    backgroundImage: soft_piano_image,
+    icon: "",
+    musicAttribute: "",
+  },
+];
